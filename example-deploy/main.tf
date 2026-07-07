be49fe5cdd15121163aa5b7605b322d0043e4927
rajneeshmahala <rajneeshmahala@gmail.com>
2026-07-07 13:41:52 +0530

    update yaml and module test

---
+++ example-deploy/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group for the example resources
+resource "azurerm_resource_group" "example" {
+  name     = "rg-webapp-example-deploy"
+  location = "East US"
+}
+
+# Create an App Service Plan
+resource "azurerm_service_plan" "example" {
+  name                = "asp-webapp-example-deploy"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "B1" # Standard dev/test tier
+}
+
+# Invoke the local Web App module
+module "web_app" {
+  source = "../"
+
+  organisation             = "ot"
+  environment              = "dev"
+  workload                 = "test"
+  location                 = azurerm_resource_group.example.location
+  name                     = "examplewebapp"
+  resource_group_name      = azurerm_resource_group.example.name
+  os_type                  = "Linux"
+  service_plan_resource_id = azurerm_service_plan.example.id
+}
